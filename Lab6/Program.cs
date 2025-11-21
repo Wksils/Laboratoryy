e536b5f36574000b9fc29934762344dc302f8983
Яна Овчинникова <Wksils@kbkit.pri>
2025-11-21 11:14:56 +0200

    Function

---
+++ Lab6/Program.cs
@@ -0,0 +1,24 @@
+﻿//функции
+//вариант 9
+//высокий уровень
+Console.WriteLine("Введите 5 строк: ");
+for (int i = 0; i < 5; i++)
+{
+    string s = Console.ReadLine();
+    bool a = Simm(s);
+    Simmetry(a);
+}
+
+
+bool Simm(string s)
+{
+    if(s.Length <= 1) return true;
+    if (s[0] == s[s.Length - 1]) return Simm (s.Substring(1, s.Length-2));
+    return false;
+}
+
+void Simmetry(bool sim)
+{
+    if (sim) Console.WriteLine("Симметричная строка");
+    else Console.WriteLine("Несимметричная строка");
+}
