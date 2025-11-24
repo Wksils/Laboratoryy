6c8aada796dd50fb3ca46607ac3040e48156141e
Яна Овчинникова <Wksils@kbkit.pri>
2025-11-24 14:23:40 +0200

    library

---
+++ Lab7/Program.cs
@@ -0,0 +1,32 @@
+﻿using Library1;
+
+//библиотеки
+//вариант 9
+//средний уровень
+
+Console.WriteLine("матрица");
+double[,] mas = new double[7, 7];
+double number;
+for (int i = 0; i < 7; i++)
+{
+    for(int j = 0; j < 7; j++)
+    {
+        if (j == 0) 
+        { 
+            Console.Write($"{mas[i, j]} "); 
+            continue; 
+        }
+        number = Math.Pow(Math.Pow(Math.Sin(i), 2) + Math.Pow(Math.Cos(j), 2), 3) - Math.Pow(-3, i + j) * Math.Log10(Math.Pow(Math.E, (i + 5) / j));
+        mas[i, j] = number;
+        Console.Write($"{mas[i, j]:F2} ");
+    }
+    Console.WriteLine();
+}
+double[] vector = Library1.Lib.Vector(mas);
+foreach (double x in vector)
+{
+    Console.Write($"{x:F2} ");
+}
+Console.WriteLine();
+double g = Library1.Lib.G(vector);
+Console.WriteLine($"g: {g:F2}");
---
+++ Library1/Class1.cs
@@ -0,0 +1,44 @@
+﻿//вариант 9
+//средний уровень
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Library1
+{
+    public class Lib
+    {
+        public static double[] Vector(double[,] mas)
+        {
+            double sum = 0;
+            double[] diagonal = new double[7];
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (i + j == 7 - 1) { diagonal[i] = mas[i, j]; }
+                    sum += mas[i, j];
+                }
+            }
+            for (int i = 0; i < diagonal.Length; i++)
+            {
+                diagonal[i] *= sum;
+            }
+            return diagonal;
+        }
+
+        public static double G(double[] mas)
+        {
+            double result = 1;
+            double realResult = 0;
+            for(int i = 1; i < 7; i++)
+            {
+                for(int j = 1; j < i; j++)
+                {
+                    result *= mas[j] / Math.E;
+                }
+                realResult += Math.Pow(mas[i], 2) - result;
+            }
+            return Math.Sqrt(Math.Abs(realResult));
+        }
+    }
+}
