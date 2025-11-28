8ea20571c6bb5874bea83a78543ff889f90ebeeb
Яна Овчинникова <Wksils@kbkit.pri>
2025-11-28 11:30:35 +0200

    struct

---
+++ Lab8/Program.cs
@@ -0,0 +1,37 @@
+﻿//перечисления и структуры
+//вариант 9
+//средний уровень
+Console.WriteLine("Введите колличество машин: ");
+int quantity = int.Parse(Console.ReadLine()!);
+Car[] car = new Car[quantity];
+for (int i = 0; i < quantity; i++)
+{
+    Console.Write("Введите марку автомобиля: ");
+    car[i].mark = Console.ReadLine()!;
+    Console.Write("Ведите производителя: ");
+    car[i].manufacturer = Console.ReadLine()!;
+    Console.Write("Введите тип автомобиля: ");
+    car[i].type = Console.ReadLine()!;
+    Console.Write("Введите год выпуска автомобиля: ");
+    car[i].year = new DateTime(int.Parse(Console.ReadLine()!),1,1);
+    Console.Write("Введите дату регистрации автомобиля: ");
+    car[i].regestration = DateTime.Parse(Console.ReadLine()!);
+    Console.WriteLine();
+}
+foreach (Car i in car)
+{
+    if (i.mark == "Ford" && i.year.Year < 2000)
+    {
+        Console.WriteLine(i.ToString());
+    }
+}
+struct Car
+{
+    public string mark;
+    public string manufacturer;
+    public string type;
+    public DateTime year;
+    public DateTime regestration;
+
+    public override string? ToString() => $"Mark:{mark}, Manufacturer:{manufacturer}, Type:{type}, Year:{year.Year}, Date:{regestration}";
+}
